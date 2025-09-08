e1616f2fcd6514363cb66691cb7b52a090382d8b
Piplikhanra14 <piplikhanra@gmail.com>
2025-09-08 22:33:37 +0530

    Streamlit donot need in this branch

--- Streamlit_Code/6_excel_chart_path.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st #streamlit import
-import pandas as pd #For any chart
-
-st.title("📂 Local Excel File Example")
-
-df = pd.read_excel("C:\\Users\\PIPLI KHANRA\Desktop\\Internship\\STUDY MATERIAL\\Cookie Types.xlsx")  # path to your local excel file
-st.dataframe(df)
-st.bar_chart(df.set_index(df.columns[0]))# df.columns[0] means it takes the first column of the dataframe as index
-# if you want to give the name of the index then use   df.set_index("Subject") 
